7dd8e9b3c0e7322669afa9dca5613a037d814782
BEZEL <mayugemicolash@yahoo.co.jp>
2026-02-17 02:43:16 +0900

    Add files via upload

---
+++ FixMissileLock/FixMissileLock_BLUS30187.asm
@@ -0,0 +1,13 @@
+.name "FixMissleLock_BLUS30187"
+.hook 0x7b3f4c ENT7b406c #entry point
+.payload 0x11fffbc #codecave
+
+ENT7b406c:
+    cmpwi cr7, r3, 0x0 #phase1 origin return check
+    bne cr7, RETURN
+    lwz r4, -0x4(r31) #get weapon structure
+    lbz r4, -0x2c(r4) #get missile flag
+    cmpwi cr7, r4, 0x1 #phase2 missile flag check
+    crnot 30, 30
+RETURN:
+    b ENT7b406c_ret
